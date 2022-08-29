03dc959d5c899932645dfda35e00ed0c280418d9
arlynmedina <arlynlinette@gmail.com>
2022-08-28 22:03:25 -0500

    Tarea

---
+++ app/app.c
@@ -0,0 +1,10 @@
+#include "pointers.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int collection[] = {4,3,2,1};
+    int count=5;
+    buuble_sort(&collection, count);
+    return 0;
+}
---
+++ lib/pointers.c
@@ -0,0 +1,68 @@
+//
+// Created by Josean Camarena on 07/07/22.
+// Implemented by: $STUDENT$
+//
+#include <stdio.h>
+#include "pointers.h"
+
+int bubble_sort(int *collection, int count)
+{
+    int times = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < count-1-i; j++)
+        {
+            if (*(collection+j)>*(collection+j+1))
+            {
+                int pivot = *(collection+j);
+                *(collection+j) = *(collection+j+1);
+                *(collection+j+1) = pivot;
+                times +=1;
+            }
+        }
+    }
+
+    return times;
+}
+
+void insertion_sort(int *collection, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        int j = i + 1;
+        while (*(collection + i) > *(collection + j) && j < count)
+        {
+            int pivot = *(collection + i);
+            *(collection + i) = *(collection + j);
+            *(collection + j) = pivot;
+            j ++;
+        }
+    }
+}
+
+void reverse(int *collection, int count)
+{
+    int last = count-1;
+
+    for (int i = 0; i < count/2;i++)
+    {
+        int pivot = *(collection+i);
+        *(collection+i) = *(collection+last);
+        *(collection+last) = pivot;
+        last -= 1;
+    }
+}
+
+int calulate_frequency(int *collection, int count, int target)
+{
+    int frecuency = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if ( *(collection+i) == target)
+        {
+            frecuency +=1;
+        }
+    }
+    return frecuency;
+}
